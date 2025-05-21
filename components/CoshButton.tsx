2d7b3ad6d7c8e1b872cbd9f369c64a8551a45c66
Liam Omen <liam.omen@student.ucll.be>
2025-05-21 08:32:33 +0200

    missing values

--- components/CoshButton.tsx
+++ components/CoshButton.tsx
@@ -1,15 +1,17 @@
-import {ReactNode} from "react";
+import { ReactNode } from 'react';
 
 interface CoshButtonProps {
-    onClick?: () => void;
-    children: ReactNode
+  onClick?: () => void;
+  children: ReactNode;
 }
 
-export default function CoshButton({onClick, children}: CoshButtonProps) {
-    return (
-        <button onClick={onClick}
-                className="rounded-[45px] bg-[#583AFF] font-inter font-medium h-[60px] w-[180px] text-[#FBF6F0] hover:shadow-lg transition-shadow">
-            {children}
-        </button>
-    )
+export default function CoshButton({ onClick, children }: CoshButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="font-inter h-[60px] w-[180px] rounded-[45px] bg-[#583AFF] font-medium text-[#FBF6F0] transition-shadow hover:shadow-lg"
+    >
+      {children}
+    </button>
+  );
 }
