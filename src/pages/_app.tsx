1a3888336ef8f522331c1b82fd1c3e2f3381d7aa
Liam Omen <liam.omen@student.ucll.be>
2025-05-21 20:12:00 +0200

    update landings page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import '@/styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import { useState } from 'react';
+import Navbar from '@/components/NavBar';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <main>
+        <div className="flex h-screen flex-col">
+          <Navbar />
+          <Component {...pageProps} />
+        </div>
+      </main>
     </QueryClientProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,15 +7,26 @@ export default function Home() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   return (
-    <>
-      <Navbar />
-      <main>
-        <div className="flex min-h-screen items-center justify-center bg-white">
-          <CoshButton onClick={() => setIsPopupOpen(true)}>Register your store!</CoshButton>
-
-          {isPopupOpen && <ScraperPopup open={isPopupOpen} onClose={() => setIsPopupOpen(false)} />}
-        </div>
-      </main>
-    </>
+    <div className="m-2 flex-1 bg-[#060026]">
+      <div className={'m-2 flex min-w-[100%] flex-col items-center pt-7'}>
+        <h1 className="text-4xl font-bold text-[#FBF6F0]">Welcome to COSH!</h1>
+        <p className="mt-4 mb-3 max-w-2xl text-lg text-[#FBF6F0]">
+          COSH.eco is your ultimate destination for sustainable and ethical fashion discovery. We
+          connect conscious consumers with eco-friendly retailers, making sustainable shopping
+          easier than ever before.
+        </p>
+        <p className="mb-3 max-w-2xl text-lg text-[#FBF6F0]">
+          Our platform provides detailed insights into stores&apos; sustainability practices, ethical
+          certifications, and brand transparency. We help you make informed decisions about your
+          fashion choices while supporting businesses that care about our planet.
+        </p>
+        <p className="mb-6 max-w-2xl text-lg text-[#FBF6F0]">
+          Join our growing community of responsible retailers and conscious consumers who are making
+          a positive impact on the fashion industry, one purchase at a time.
+        </p>
+        <CoshButton onClick={() => setIsPopupOpen(true)}>Register your store now!</CoshButton>
+        {isPopupOpen && <ScraperPopup open={isPopupOpen} onClose={() => setIsPopupOpen(false)} />}
+      </div>
+    </div>
   );
 }
