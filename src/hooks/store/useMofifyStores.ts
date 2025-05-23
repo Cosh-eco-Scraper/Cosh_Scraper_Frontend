4cc390b9e47480347a4b5dec991f3fa9e6f2bcc5
Liam Omen <liam.omen@student.ucll.be>
2025-05-23 20:10:37 +0200

    latest commit

--- src/hooks/store/useMofifyStores.ts
+++ src/hooks/store/useMofifyStores.ts
@@ -1,30 +1,29 @@
-import {queryClient} from '@/config/queryClient';
-import {useMutation} from '@tanstack/react-query';
-import {UpdateResponse} from '@/domain/UpdateResponse';
-import {CreateStore} from '@/domain/Store';
+import { queryClient } from '@/config/queryClient';
+import { useMutation } from '@tanstack/react-query';
+import { CreateStore } from '@/domain/Store';
 import StoreService from '@/service/StoreService';
-import {queryKeys} from '@/hooks/queryKeys';
-import {CreateResponse} from "@/domain/CreateResponse";
+import { queryKeys } from '@/hooks/queryKeys';
+import { CreateResponse } from '@/domain/CreateResponse';
 
 export default function useModifyStores() {
-    const {
-        data: storeResponse,
-        mutateAsync: createStore,
-        isSuccess: isSuccessCreateStore,
-        isError: isErrorCreateStore,
-        isPending: isPendingCreateStore,
-    } = useMutation<CreateResponse, unknown, CreateStore>({
-        mutationFn: (store: CreateStore) => StoreService.createStore(store),
-        onSuccess: async () => {
-            queryClient.invalidateQueries({queryKey: queryKeys.getAllStoresKey()});
-        },
-    });
+  const {
+    data: storeResponse,
+    mutateAsync: createStore,
+    isSuccess: isSuccessCreateStore,
+    isError: isErrorCreateStore,
+    isPending: isPendingCreateStore,
+  } = useMutation<CreateResponse, unknown, CreateStore>({
+    mutationFn: (store: CreateStore) => StoreService.createStore(store),
+    onSuccess: async () => {
+      queryClient.invalidateQueries({ queryKey: queryKeys.getAllStoresKey() });
+    },
+  });
 
-    return {
-        storeResponse,
-        createStore,
-        isSuccessCreateStore,
-        isErrorCreateStore,
-        isPendingCreateStore
-    };
+  return {
+    storeResponse,
+    createStore,
+    isSuccessCreateStore,
+    isErrorCreateStore,
+    isPendingCreateStore,
+  };
 }
