5ffa22a21850eff1fa16af0ac2809f4c681c9dc2
Liam Omen <liam.omen@student.ucll.be>
2025-05-23 12:30:09 +0200

    brands will be added later

--- src/hooks/queryKeys.ts
+++ src/hooks/queryKeys.ts
@@ -1,6 +1,7 @@
 export const queryKeys = {
   getStoreKey: (id: number) => ['store', id],
   getAllStoresKey: () => ['stores'],
+  getLocationsKey: () => ['locations'],
   getStoreBrandsKey: (id: number) => ['brands', id],
   getStoreHoursKey: (id: number) => ['hours', id],
 };
