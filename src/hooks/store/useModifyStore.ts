f29335f7cb6a846fd2f8bb6569327e64fb1a2238
Liam Omen <liam.omen@student.ucll.be>
2025-05-23 11:52:55 +0200

    useModifyStores

--- src/hooks/store/useModifyStore.ts
+++ src/hooks/store/useModifyStore.ts
@@ -1,26 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
-import { UpdateStore } from '@/domain/Store';
-import StoreService from '@/service/StoreService';
-import { UpdateResponse } from '@/domain/UpdateResponse';
-import { queryClient } from '@/config/queryClient';
-import { queryKeys } from '@/hooks/queryKeys';
+import {useMutation} from "@tanstack/react-query";
+import {UpdateStore} from "@/domain/Store";
+import StoreService from "@/service/StoreService";
+import {UpdateResponse} from "@/domain/UpdateResponse";
+import {queryClient} from "@/config/queryClient";
+import {queryKeys} from "@/hooks/queryKeys";
 
 export default function useModifyStore(id: number) {
-  const {
-    mutate: updateStore,
-    isSuccess: isSuccessUpdateStore,
-    isError: isErrorUpdateStore,
-  } = useMutation<UpdateResponse, unknown, UpdateStore>({
-    mutationFn: (store: UpdateStore) => StoreService.updateStore(id, store),
-    onSuccess: async () => {
-      console.log('Store updated successfully');
-      await queryClient.invalidateQueries({ queryKey: queryKeys.getStoreKey(id) });
-    },
-  });
-
-  return {
-    updateStore,
-    isSuccessUpdateStore,
-    isErrorUpdateStore,
-  };
+    const {mutate: updateStore, isSuccess: isSuccessUpdateStore, isError: isErrorUpdateStore}
+        = useMutation<UpdateResponse>({
+        mutationFn: ({id, store}: { id: number, store: UpdateStore }) => StoreService.updateStore(id, store),
+        onSuccess: () => {
+            console.log('Store updated successfully')
+            queryClient.invalidateQueries({queryKey: queryKeys.getStoreKey(id)})
+        }
+    })
 }
