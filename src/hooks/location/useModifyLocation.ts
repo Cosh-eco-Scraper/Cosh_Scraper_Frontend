d4812a4639471d1f88b9e39360c83527104dde93
Liam Omen <liam.omen@student.ucll.be>
2025-05-23 12:07:11 +0200

    use modify location

--- src/hooks/location/useModifyLocation.ts
+++ src/hooks/location/useModifyLocation.ts
@@ -1,26 +1,29 @@
-import { useMutation } from '@tanstack/react-query';
-import { UpdateResponse } from '@/domain/UpdateResponse';
-import { queryClient } from '@/config/queryClient';
-import { queryKeys } from '@/hooks/queryKeys';
-import LocationService from '@/service/LocationService';
-import { CreateLocation } from '@/domain/Location';
+import {useMutation} from '@tanstack/react-query';
+import {UpdateStore} from '@/domain/Store';
+import StoreService from '@/service/StoreService';
+import {UpdateResponse} from '@/domain/UpdateResponse';
+import {queryClient} from '@/config/queryClient';
+import {queryKeys} from '@/hooks/queryKeys';
+import LocationService from "@/service/LocationService";
+import {CreateLocation} from "@/domain/Location";
 
 export default function useModifyLocation(id: number) {
-  const {
-    mutate: updateLocation,
-    isSuccess: isSuccessUpdateLocation,
-    isError: isErrorUpdateLocation,
-  } = useMutation<UpdateResponse, unknown, CreateLocation>({
-    mutationFn: (location: CreateLocation) => LocationService.updateLocation(id, location),
-    onSuccess: async () => {
-      console.log('Store updated successfully');
-      await queryClient.invalidateQueries({ queryKey: queryKeys.getStoreKey(id) });
-    },
-  });
+    const {
+        mutate: updateLocation,
+        isSuccess: isSuccessUpdateLocation,
+        isError: isErrorUpdateLocation,
+    } = useMutation<UpdateResponse, unknown, CreateLocation>({
+        mutationFn: (location: CreateLocation) =>
+            LocationService.updateLocation(id, location),
+        onSuccess: async () => {
+            console.log('Store updated successfully');
+            await queryClient.invalidateQueries({queryKey: queryKeys.getStoreKey(id)});
+        },
+    });
 
-  return {
-    updateLocation,
-    isSuccessUpdateLocation,
-    isErrorUpdateLocation,
-  };
+    return {
+        updateLocation,
+        isSuccessUpdateLocation,
+        isErrorUpdateLocation,
+    }
 }
--- src/hooks/store/useMofifyStores.ts
+++ src/hooks/store/useMofifyStores.ts
@@ -1,18 +1,26 @@
-import {queryClient} from "@/config/queryClient";
+import {queryClient} from '@/config/queryClient';
+import {useMutation} from "@tanstack/react-query";
+import {UpdateResponse} from "@/domain/UpdateResponse";
+import {CreateStore, UpdateStore} from "@/domain/Store";
+import StoreService from "@/service/StoreService";
+import {queryKeys} from "@/hooks/queryKeys";
 
 export default function useModifyStores() {
-    const {mutate: createStore, isSuccess: isSuccessCreateStore, isError: isErrorCreateStore}
-        = useMutation<UpdateResponse>({
-        mutationFn: ({store}: { id: number, store: UpdateStore }) => StoreService.createStore(store),
-        onSuccess: () => {
-            console.log('Store created successfully')
-            queryClient.invalidateQueries({queryKey: queryKeys.getAllStoresKey()})
-        }
-    })
+    const {
+        mutate: createStore,
+        isSuccess: isSuccessCreateStore,
+        isError: isErrorCreateStore,
+    } = useMutation<UpdateResponse, unknown, CreateStore>({
+        mutationFn: (store: CreateStore) => StoreService.createStore(store),
+        onSuccess: async () => {
+            console.log('Store created successfully');
+            await queryClient.invalidateQueries({queryKey: queryKeys.getAllStoresKey()});
+        },
+    });
 
     return {
         createStore,
         isSuccessCreateStore,
         isErrorCreateStore,
-    }
+    };
 }
