ca7d8c0eaf784c7a4824721e9e1aedae317dc4e2
Liam Omen <liam.omen@student.ucll.be>
2025-05-23 20:09:33 +0200

    latest commit

--- src/components/ScraperPopUp.tsx
+++ src/components/ScraperPopUp.tsx
@@ -1,68 +1,113 @@
 import router from 'next/router';
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import {useForm} from 'react-hook-form';
+import useModifyStores from "@/hooks/store/useMofifyStores";
+import {CreateStore} from "@/domain/Store";
 
 interface MyPopupProps {
-  open: boolean;
-  onClose: () => void;
+    open: boolean;
+    onClose: () => void;
 }
 
-const ScraperPopup: React.FC<MyPopupProps> = ({ onClose }) => {
-  const [isLoading, setIsLoading] = useState(false);
 
-  const ConfirmScraper = (e: React.FormEvent) => {
-    e.preventDefault();
-    setIsLoading(true);
+const ScraperPopup: React.FC<MyPopupProps> = ({onClose}) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const {register, handleSubmit} = useForm<CreateStore>();
+    const {createStore, isPendingCreateStore, isSuccessCreateStore, storeResponse} = useModifyStores();
 
-    setTimeout(async () => {
-      await router.push('/info/1');
-    }, 1500);
-  };
+    const onSubmit = async (data: CreateStore) => {
+        try {
+            await createStore(data)
+        } catch {
+            console.error('create store failed')
+        }
+    };
 
-  return (
-    <div className="bg-grey bg-opacity-50 fixed inset-0 z-50 flex items-center justify-center">
-      <div className="w-96 rounded bg-white p-6 shadow-lg">
-        <h2 className="mb-4 text-xl font-bold text-black">Join us</h2>
+    useEffect(() => {
+        if (isPendingCreateStore) {
+            setIsLoading(true);
+        } else {
+            setIsLoading(false);
+        }
 
-        <form className="space-y-4">
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Name of store</label>
-            <input
-              type="text"
-              className="border-black-300 mt-1 block w-full rounded-md border p-2 text-black placeholder-gray-400 shadow-sm"
-              placeholder="Enter your store name"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">URL to your website</label>
-            <input
-              type="text"
-              className="mt-1 block w-full rounded-md border border-gray-300 p-2 text-black placeholder-gray-400 shadow-sm"
-              placeholder="Enter the url to your website"
-            />
-          </div>
+        console.log('Success:', isSuccessCreateStore, 'Response:', storeResponse);
 
-          <button
-            onClick={onClose}
-            className="padding-100 mt-4 rounded bg-blue-500 px-4 py-2 text-white"
-          >
-            Close
-          </button>
+        if (isSuccessCreateStore && storeResponse?.id) {
 
-          <button
-            onClick={ConfirmScraper}
-            type="submit"
-            className="mt-4 rounded bg-green-500 px-4 py-2 text-white"
-          >
-            Submit
-          </button>
+            console.log('Response id:', storeResponse.id);
+            setTimeout(() => {
+                router.push(`stores/${storeResponse.id}/modify`);
+                onClose();
+            }, 100); // or even 0ms
+        }
+    }, [isPendingCreateStore, isSuccessCreateStore, storeResponse]);
 
-          {isLoading && (
-            <p className="mt-4 text-center font-medium text-gray-600">Loading, please wait...</p>
-          )}
-        </form>
-      </div>
-    </div>
-  );
+
+    const handleBackgroundClick = (e: React.MouseEvent) => {
+        if (e.target === e.currentTarget) {
+            onClose();
+        }
+    };
+
+    return (
+        <div onClick={handleBackgroundClick}
+             className="bg-grey bg-opacity-50 fixed inset-0 z-50 flex items-center justify-center">
+            <div className="w-96 rounded bg-white p-6 shadow-lg">
+                <h2 className="mb-4 text-xl font-bold text-black">Join us</h2>
+
+                <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">Name of store</label>
+                        <input
+                            {...register('name')}
+                            type="text"
+                            className="border-black-300 mt-1 block w-full rounded-md border p-2 text-black placeholder-gray-400 shadow-sm"
+                            placeholder="Example store"
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">City</label>
+                        <input
+                            {...register('location')}
+                            type="text"
+                            className="mt-1 block w-full rounded-md border border-gray-300 p-2 text-black placeholder-gray-400 shadow-sm"
+                            placeholder="Eindhoven"
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">URL</label>
+                        <input
+                            {...register('url')}
+                            type="text"
+                            className="mt-1 block w-full rounded-md border border-gray-300 p-2 text-black placeholder-gray-400 shadow-sm"
+                            placeholder="https://www.example.com"
+                        />
+                    </div>
+
+                    <div className="flex justify-between">
+                        <button
+                            type="submit"
+                            className="mt-4 rounded bg-green-500 px-4 py-2 text-white"
+                        >
+                            Submit
+                        </button>
+
+                        <button
+                            onClick={onClose}
+                            type="button"
+                            className="padding-100 mt-4 rounded bg-blue-500 px-4 py-2 text-white"
+                        >
+                            Close
+                        </button>
+                    </div>
+
+                    {isLoading && (
+                        <p className="mt-4 text-center font-medium text-gray-600">Loading, please wait...</p>
+                    )}
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default ScraperPopup;
