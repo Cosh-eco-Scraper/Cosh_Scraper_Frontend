e0f81123d9ac55b8ba04ecd0a85879cbe3c5625a
Liam Omen <liam.omen@student.ucll.be>
2025-05-26 09:42:25 +0200

    prettier

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -2,13 +2,13 @@
 import axios from 'axios';
 
 const baseURL =
-    typeof window === 'undefined'
-        ? process.env.API_URL  // use private one on server
-        : process.env.NEXT_PUBLIC_API_URL;  // public for client
+  typeof window === 'undefined'
+    ? process.env.API_URL // use private one on server
+    : process.env.NEXT_PUBLIC_API_URL; // public for client
 
 const axiosInstance = axios.create({
-    baseURL: `${baseURL}/api/`,
-    headers: {'Content-Type': 'application/json'},
+  baseURL: `${baseURL}/api/`,
+  headers: { 'Content-Type': 'application/json' },
 });
 
 export default axiosInstance;
