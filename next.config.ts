3d45eb036eb24bee58f46038cf6f9eea185ac3c8
Liam Omen <liam.omen@student.ucll.be>
2025-05-26 16:45:42 +0200

    change in next.config

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
   // Variabelen moeten beginnen met 'NEXT_PUBLIC_' om in de browser beschikbaar te zijn.
   env: {
     // Hier wordt de waarde van de omgevingsvariabele die je in Render instelt, opgehaald.
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
 
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
 // Als je 'undefined' ziet, is de variabele niet correct ingesteld in Render of niet zichtbaar tijdens de build.
 console.log(
   'NEXT_PUBLIC_BACKEND_URL in next.config.js (BUILD LOG):',
-  process.env.NEXT_PUBLIC_BACKEND_URL
+  process.env.NEXT_PUBLIC_API_URL
 );
 
 export default nextConfig;
