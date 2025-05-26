8b3be9a2cf842250f6e47631382d011d55ca24e6
Liam Omen <liam.omen@student.ucll.be>
2025-05-26 09:40:27 +0200

    disable main push

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,9 +1,14 @@
 // lib/axiosInstance.js
 import axios from 'axios';
 
+const baseURL =
+    typeof window === 'undefined'
+        ? process.env.API_URL  // use private one on server
+        : process.env.NEXT_PUBLIC_API_URL;  // public for client
+
 const axiosInstance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/`,
-  headers: { 'Content-Type': 'application/json' },
+    baseURL: `${baseURL}/api/`,
+    headers: {'Content-Type': 'application/json'},
 });
 
 export default axiosInstance;
