a3c12592ade0928cff96adab26c03b2f1afdb6a2
Liam Omen <liam.omen@student.ucll.be>
2025-05-26 11:01:56 +0200

    add env to docker build

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,9 @@ const nextConfig: NextConfig = {
 // Deze console.log wordt uitgevoerd TIJDENS DE BUILD op de Render server.
 // Controleer de BUILD LOGS van je Next.js service op Render om te zien welke waarde hier wordt opgepikt.
 // Als je 'undefined' ziet, is de variabele niet correct ingesteld in Render of niet zichtbaar tijdens de build.
-console.log("NEXT_PUBLIC_BACKEND_URL in next.config.js (BUILD LOG):", process.env.NEXT_PUBLIC_BACKEND_URL);
+console.log(
+  'NEXT_PUBLIC_BACKEND_URL in next.config.js (BUILD LOG):',
+  process.env.NEXT_PUBLIC_BACKEND_URL
+);
 
 export default nextConfig;
