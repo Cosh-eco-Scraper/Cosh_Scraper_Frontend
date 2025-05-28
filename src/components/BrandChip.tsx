7ac9816869f0b37098c5a6796d3e356231664046
MatteoBoulanger-ucll <matteo.boulanger@student.ucll.be>
2025-05-28 11:29:01 +0200

    azert

--- src/components/BrandChip.tsx
+++ src/components/BrandChip.tsx
@@ -1,4 +1,4 @@
-import Label from "./Label";
+import Label from './Label';
 
 interface BrandChipProps {
   name: string;
--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,18 +1,21 @@
 interface Label {
-    label: string;
+  label: string;
 }
 
 export default function Label({ label }: Label) {
-    return (
-        <span className={`flex items-center gap-1 rounded-full px-2 py-1 text-xs text-black shadow ${label === 'A'
-            ? 'bg-[#3aff82]'
-            : label === 'B'
-                ? 'bg-[#ffcc00]'
-                : label === 'C'
-                    ? 'bg-[#ff8800]'
-                    : null
-            }`}>
-            {label}
-        </span>
-    );
+  return (
+    <span
+      className={`flex items-center gap-1 rounded-full px-2 py-1 text-xs text-black shadow ${
+        label === 'A'
+          ? 'bg-[#3aff82]'
+          : label === 'B'
+            ? 'bg-[#ffcc00]'
+            : label === 'C'
+              ? 'bg-[#ff8800]'
+              : null
+      }`}
+    >
+      {label}
+    </span>
+  );
 }
