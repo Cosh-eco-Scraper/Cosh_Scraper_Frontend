0d4041dd279f4b77892a8c2f130751114647422b
MatteoBoulanger-ucll <matteo.boulanger@student.ucll.be>
2025-05-28 11:28:22 +0200

    label component made

--- src/components/BrandChip.tsx
+++ src/components/BrandChip.tsx
@@ -1,12 +1,15 @@
+import Label from "./Label";
+
 interface BrandChipProps {
   name: string;
   label: string;
 }
 
-export default function BrandChip({ name }: BrandChipProps) {
+export default function BrandChip({ name, label }: BrandChipProps) {
   return (
     <span className="flex items-center gap-1 rounded-full bg-[#583AFF] px-3 py-1.5 text-sm text-white shadow">
       {name}
+      {label && <Label label={label} />}
     </span>
   );
 }
