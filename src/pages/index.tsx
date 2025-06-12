cbffd9504c565e5372ed06155214e97c5fbe0c9c
Rafik <rafik.anamse@student.ucll.be>
2025-06-12 16:20:30 +0200

    formaté

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,13 +45,13 @@ export default function Home() {
                   Welcome to COSH!
                 </h1>
                 <p className="mt-4 mb-3 max-w-2xl text-lg text-[#FBF6F0]">
-                  COSH! is your ultimate destination for sustainable and ethical fashion discovery. We
-                  connect conscious consumers with eco-friendly retailers, making sustainable shopping
-                  easier than ever before.
+                  COSH! is your ultimate destination for sustainable and ethical fashion discovery.
+                  We connect conscious consumers with eco-friendly retailers, making sustainable
+                  shopping easier than ever before.
                 </p>
                 <p className="mb-6 max-w-2xl text-lg text-[#FBF6F0]">
-                  Join our growing community of responsible retailers and conscious consumers who are
-                  making a positive impact on the fashion industry, one purchase at a time.
+                  Join our growing community of responsible retailers and conscious consumers who
+                  are making a positive impact on the fashion industry, one purchase at a time.
                 </p>
                 <CoshButton onClick={() => setIsPopupOpen(true)}>
                   Register your store now!
@@ -82,9 +82,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        {isPopupOpen && (
-          <ScraperPopup open={isPopupOpen} onClose={() => setIsPopupOpen(false)} />
-        )}
+        {isPopupOpen && <ScraperPopup open={isPopupOpen} onClose={() => setIsPopupOpen(false)} />}
       </div>
     </>
   );
